c989b62cd17d09acbd3ec1e2d46c7d0b08858f5f
Nikolai <skofenkonikolai@mail.ru>
2023-04-06 19:08:10 +0300

    1

---
+++ oop2.py
@@ -0,0 +1,26 @@
+from pprint import  pprint
+def dish_recipes():
+    with open('cook_book.txt', encoding='utf-8') as file:
+        cook_book = {}
+        for line in file:
+            dish_name = line.strip()
+            ingredients_count = int(file.readline())
+            ingredients = []
+            for _ in range(ingredients_count):
+                name, quantity, measure = file.readline().strip().split(' | ')
+                ingredients.append({'name': name, 'quantity': quantity, 'measure': measure})
+            file.readline()
+            cook_book[dish_name] = ingredients
+    return cook_book
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredient = {}
+    for dish in dishes:
+        for i in range(len(dish_recipes()[dish])):
+            name, quantity, measure = dish_recipes()[dish][i].values()
+            if name not in ingredient:
+                ingredient[name] = {'measure': measure, 'quantity': int(quantity) * person_count}
+            else:
+                ingredient[name]['quantity'] += int(quantity) * person_count
+    pprint(ingredient)
